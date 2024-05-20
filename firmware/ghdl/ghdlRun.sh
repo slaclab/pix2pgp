ba6632756da797d932e4787cc65ce692a3e62126
My Name <myemail@slac.stanford.edu>
2024-05-20 14:55:04 -0700

    fix surf pkg dir and ghdl cmd error in slac servers

--- firmware/ghdl/ghdlRun.sh
+++ firmware/ghdl/ghdlRun.sh
@@ -46,7 +46,12 @@ TB=${TB_DIR}/*Tb.vhd
 FST=${GHDL_DIR}/*.fst
 OUT=${GHDL_DIR}/*.o
 
+#############################
 GHDL_CMD="ghdl-llvm"
+# check if your ghdl version is < 1.0.0; if it is, you need to use a downloaded tarball from https://github.com/ghdl/ghdl/releases
+# example below...
+# GHDL_CMD="/home/cbakalis/Downloads/bin/ghdl"
+#############################
 GHDL_GLBL_FLAGS="--ieee=standard -fexplicit -fsynopsys"
 GHDL_STD_FLAG="--std=93c"
 
