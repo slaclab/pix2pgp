f698ef321b7f14b00f1dd65c7887a2cc678fab06
Christos Bakalis <cbakalis@slac.stanford.edu>
2025-04-24 18:31:30 -0700

    housekeeping

---
+++ software/scripts/laneDumpParser.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import click
+import argparse
+
+import setupLibPaths
+import pix2pgp
+
+# Set the argument parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--dataFile",
+    type     = str,
+    required = False,
+    default  = '../firmware/ghdl/pix2pgpRxDataDump.dat',
+    help     = "Default data file",
+)
+
+parser.add_argument(
+    "--hitDecode",
+    action   ="store_true",
+    help     = "Set to true if data in testbench have upper 10 bits is ColID and lower as a cnt",
+)
+
+parser.add_argument(
+    "--asicType",
+    type     = str,
+    required = False,
+    default  = 'SparkPixS',
+    help     = "options: SparkPixS, SparkPixT",
+)
+
+# Get the arguments
+args = parser.parse_args()
+
+#################################################################
+if __name__ == "__main__":
+    _file = args.dataFile
+
+    if not(os.path.isfile(_file)):
+        click.secho(f"[ERROR]: {_file} not found!", bg='red')
+        sys.exit()
+
+    if args.asicType != "SparkPixS" and args.asicType != "SparkPixT":
+        click.secho(f"[ERROR]: asicType flag not set properly! options: SparkPixS, SparkPixT", bg='red')
+        sys.exit()
+
+    with open(_file) as f:
+        _dataArray = [int(line.rstrip('\n'), 16) for line in f]
+
+    # laneParser = pix2pgp.LaneData(asicType=args.asicType, verbose=True)
+    # asicParser.formatter(data=_dataArray, dataLen=len(_dataArray))
