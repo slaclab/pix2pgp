d5434e4b8be9017db12d1b926be88688bb99d2c1
Christos Bakalis <cbakalis@slac.stanford.edu>
2025-05-06 17:30:02 -0700

    enhance: ruckus has necessary files for fpga design

--- firmware/fpga/ruckus.tcl
+++ firmware/fpga/ruckus.tcl
@@ -2,7 +2,9 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-loadSource -lib pix2pgp -dir           "$::DIR_PATH/rtl"
+loadSource -lib pix2pgp          -path "$::DIR_PATH/../asic/rtl/pkg/Pix2PgpPkg.vhd"
+loadSource -lib pix2pgp          -path "$::DIR_PATH/../asic/rtl/Pix2PgpWatchdog.vhd"
+loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl"
 loadSource -lib pix2pgp -sim_only -dir "$::DIR_PATH/tb"
 
 # Load Source Code
