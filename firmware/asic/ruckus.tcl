81553b6fcc33ad8f0e162c799fa070aa690940ff
Christos Bakalis <cbakalis@slac.stanford.edu>
2025-10-14 08:52:01 -0700

    modify ruckus scripts for vivado

--- firmware/asic/ruckus.tcl
+++ firmware/asic/ruckus.tcl
@@ -3,10 +3,11 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 # note that synopsysFifo is sourced in the Vivado-based flow
-loadSource -lib pix2pgp -dir "$::DIR_PATH/rtl"
-loadSource -lib pix2pgp -dir "$::DIR_PATH/rtl/asicTop"
-loadSource -lib pix2pgp -dir "$::DIR_PATH/rtl/synopsysFifo"
-loadSource -lib pix2pgp -dir "$::DIR_PATH/rtl/pkg"
+loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl"
+loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl/asicTop"
+loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl/synopsysFifo"
+loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl/pkg"
+loadSource -lib pix2pgp -sim_only -dir "$::DIR_PATH/tb"
 
 # Load Simulation
 loadSource -lib pix2pgp -sim_only -dir "$::DIR_PATH/tb"
