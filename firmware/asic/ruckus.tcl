cb38466592d878f41f8170d1778522871326b8a3
Christos Bakalis <cbakalis@slac.stanford.edu>
2026-01-21 13:05:13 -0800

    update submodule pointers

--- firmware/asic/ruckus.tcl
+++ firmware/asic/ruckus.tcl
@@ -1,6 +1,17 @@
 # Load RUCKUS library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+# Check for submodule tagging
+if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
+   if { [SubmoduleCheck {ruckus} {4.22.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.67.0} ] < 0 } {exit -1}
+} else {
+   puts "\n\n*********************************************************"
+   puts "OVERRIDE_SUBMODULE_LOCKS != 0"
+   puts "Ignoring the submodule locks in pix2pgp/ruckus.tcl"
+   puts "*********************************************************\n\n"
+}
+
 # Load Source Code
 loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl"
 loadSource -lib pix2pgp           -dir "$::DIR_PATH/rtl/asicTop"
--- firmware/fpga/ruckus.tcl
+++ firmware/fpga/ruckus.tcl
@@ -1,6 +1,17 @@
 # Load RUCKUS library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
+# Check for submodule tagging
+if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
+   if { [SubmoduleCheck {ruckus} {4.22.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.67.0} ] < 0 } {exit -1}
+} else {
+   puts "\n\n*********************************************************"
+   puts "OVERRIDE_SUBMODULE_LOCKS != 0"
+   puts "Ignoring the submodule locks in pix2pgp/ruckus.tcl"
+   puts "*********************************************************\n\n"
+}
+
 # Load Source Code
 loadSource -lib pix2pgp -path "$::DIR_PATH/../asic/rtl/pkg/Pix2PgpAsicPkg.vhd"
 loadSource -lib pix2pgp -path "$::DIR_PATH/../asic/rtl/pkg/Pix2PgpPkg.vhd"
