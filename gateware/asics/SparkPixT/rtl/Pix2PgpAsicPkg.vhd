2b037db92fbea87a31a1c3838ca1487ab12be923
Christos Bakalis <cbakalis@slac.stanford.edu>
2026-06-11 10:46:43 -0700

    remove unused package constants

--- gateware/asics/SparkPixT/rtl/Pix2PgpAsicPkg.vhd
+++ gateware/asics/SparkPixT/rtl/Pix2PgpAsicPkg.vhd
@@ -60,8 +60,6 @@ package Pix2PgpAsicPkg is
    --
    constant EVAL_SOF_C  : boolean := True;
    constant EVAL_EOFE_C : boolean := True;
-
-   constant EVAL_DUMMY_MAX_C : natural := 0;
    --
    -- **************************************************************************
 
